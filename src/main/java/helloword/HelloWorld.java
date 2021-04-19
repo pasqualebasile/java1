381ae732b84b846b5f8fad75862fe70009995f58
Pasquale <pasquale.basile@infocube.it>
2021-04-19 11:33:24 +0200

    Iniziale

---
+++ src/main/java/helloword/HelloWorld.java
@@ -0,0 +1,23 @@
+package helloword;
+
+public class HelloWorld {
+  private static String HELLOWORLD="Salve, Mondo";
+
+  public static void main(String[] args) {
+    String SALVEMONDO="Salve, Mondo";
+
+    /*
+     Questo è un commento
+     */
+
+    // Questo è un commento
+    System.out.println(HELLOWORLD);
+    System.out.println(SALVEMONDO);
+
+    // Variabili
+    Integer x ;
+    x = 10;
+    System.out.println(HELLOWORLD + x.toString());
+  }
+
+}
